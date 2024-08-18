76d324d58397a8672cb92948eac2c57c52f8e9f9
Bensoli-Inc <benswambua96@gmail.com>
2024-08-18 14:28:21 +0300

    overloading

---
+++ Methods/Main.java
@@ -0,0 +1,17 @@
+class Calculator
+{
+    public int add(int n1, int n2)
+    {
+       return n1 + n2;
+    }
+}
+
+public class Main
+{
+    public static void main(String[] a)
+    {  
+        Calculator obj = new Calculator();
+        int r1 = obj.add(2, 3);
+        System.out.println(r1);
+    }
+}
