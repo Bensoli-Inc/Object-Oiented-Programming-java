b9d2bfd3b92bed1c9f6d862a9abee4062d89086d
Bensoli-Inc <benswambua96@gmail.com>
2024-08-20 08:26:24 +0300

    static methods

--- Static/StaticMethod/Main.java
+++ Static/StaticMethod/Main.java
@@ -3,23 +3,17 @@ class Mobile
 {
     String brand;
     int price;
-    static String name;
+    static String name; 
 
-    //static block
-    static
-    {
-        name = "phone";
-        System.out.println("static block called...");
-    }
-    public Mobile()
-    {
-        brand = "";
-        price = 200;
-        System.out.println("constructors called...");
-    }
+    
     public void show()
     {
         System.out.println(brand + ": " + price + " : " + name);
+    }
+
+    public static void show1(Mobile obj)
+    {
+        System.out.println(obj.brand + ": " + obj.price + " : " + name);
     }
 }
 
@@ -30,8 +24,16 @@ public class Main
         Mobile obj1 = new Mobile();
         obj1.brand = "Apple";
         obj1.price = 1500;
-        Mobile.name = "SmartPhone"; 
+        Mobile.name = "SmartPhone"; // we can access the property name directly without calling any object by just calling the class
         
         Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 2000;
+        Mobile.name = "SmartPhone";
+        
+        obj1.show();
+        obj2.show();
+
+        Mobile.show1(obj1); //u can call static methods with help of classname
     }
 }
