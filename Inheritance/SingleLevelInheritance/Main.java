4e7c23b2f5114a4af3c4ed70bcb4d7ca15cceb44
Bensoli-Inc <benswambua96@gmail.com>
2024-08-20 12:05:53 +0300

    single level inheritance

---
+++ Inheritance/SingleLevelInheritance/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main
+{
+    public static void main(String[] a)
+    {        
+        AdvancedCalc obj1 = new AdvancedCalc();
+        int sum1 = obj1.add(3,5);
+        int diff1 = obj1.sub(8,3);
+        int multi = obj1.multiply(3,5);
+        int div = obj1.div(8,5);
+
+        System.out.println("sum :" + sum1);
+        System.out.println("Diff :" + diff1);
+        System.out.println("Multiplication :" + multi);
+        System.out.println("Qoutient :" + div);
+    }
+}
