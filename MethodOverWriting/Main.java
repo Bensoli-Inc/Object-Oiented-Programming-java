bfa1a1b0fd3530374f18a72d35cefe2c371f8d7e
Bensoli-Inc <benswambua96@gmail.com>
2024-08-20 12:33:19 +0300

    method overwriting

---
+++ MethodOverWriting/Main.java
@@ -0,0 +1,27 @@
+ class A
+{
+    public void show()
+    {
+        System.out.println("in A show");
+    }
+
+    public void config()
+    {
+        System.out.println("in A config");
+    }
+}
+
+ class B extends A
+{
+
+}
+
+public class Main
+{
+    public static void main(String[] a)
+    {
+        B obj = new B();
+        obj.show();
+        obj.config();
+    }
+}
