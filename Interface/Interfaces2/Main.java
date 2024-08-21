d8b2440f9095c2bd12f0f2ff7685fa74d998b252
Bensoli-Inc <benswambua96@gmail.com>
2024-08-21 13:12:41 +0300

    interface

---
+++ Interface/Interfaces2/Main.java
@@ -0,0 +1,38 @@
+interface A 
+{
+    // public abstract void show(); --no need tomention public abstract
+    // public abstract void config();
+    void show();
+    void config();
+
+//all variables in interface are final and static- must assign
+    int age = 22;
+    String area = "Juja";
+}
+
+class B implements A
+{
+    public void show()
+    {
+        System.out.println("in show");
+    }
+
+    public void config()
+    {
+        System.out.println("in config");
+    }
+}
+
+public class Main
+{
+    public static void main(String[] a)
+    {
+       B obj;
+       obj = new B();
+       obj.show();
+       obj.config();
+
+       System.out.println(A.area);
+       A.area = "NAIROBI"; //error
+    }
+}
