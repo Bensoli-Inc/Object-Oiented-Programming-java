f3d30da5030ab0e1f6438bea1c8e1f9df545d8c5
Bensoli-Inc <benswambua96@gmail.com>
2024-08-22 09:17:42 +0300

    arithmetic exceptions arrayOutOfBoundsExeception Exception Throwable

--- Exceptions/HandlingExceptions/Main.java
+++ Exceptions/HandlingExceptions/Main.java
@@ -12,19 +12,31 @@ public class Main
         // int j = 18/i; //critical statement - arithmetic exception - execution stopped
 
         //HANDLING THE EXCEPTION
-        int i = 0;
+        int i = 2;
         int j = 0;
+
+        int nums[] = new int[5];
+        String str = null;
         try
         {
             j=18/i;
+            System.out.println(nums[3]); //out of bounds exception
+            System.out.println(str.length());
         }
-        catch(Exception e) //Exception is a class in java
+        catch(ArithmeticException e) //Exception is a class in java
         {
-            System.out.println("Something went wrong");
+            System.out.println("Cannot divide by zero. " + e);
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Stay in your array limit. " + e);
+        }
+        catch(Exception e)
+        {
+
+            System.out.println("Something went wrong. " + e);
         }
 
         System.out.println(j);
-        System.out.println("Bye");
-
     }
 }
