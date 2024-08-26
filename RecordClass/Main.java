873759bd40a76c6eceb1c5e144b84907d06ef898
Bensoli-Inc <benswambua96@gmail.com>
2024-08-26 10:26:41 +0300

    record class with variables and constructors at once

--- RecordClass/Main.java
+++ RecordClass/Main.java
@@ -1,10 +1,10 @@
-record Alien(int id, String name) {}
+record Alien(int id, String name) {} //declared class with variables and constructors at once
 
 public class Main
 {
     public static void main(String[] args)
     {
-        Alien a1 = new Alien(3, "peter");
+        Alien a1 = new Alien(3, "peter"); //immutable. created just to crarry data
         System.out.println(a1);
     }
 }
