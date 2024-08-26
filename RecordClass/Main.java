bab5065360124741c5766a45c9db7f56ae5e9193
Bensoli-Inc <benswambua96@gmail.com>
2024-08-26 10:24:06 +0300

    record class with variables and constructors at once

---
+++ RecordClass/Main.java
@@ -0,0 +1,10 @@
+record Alien(int id, String name) {}
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Alien a1 = new Alien(3, "peter");
+        System.out.println(a1);
+    }
+}
